9282fb6375202b4da65cd62f292331be97a1301e
MWBradshaw <mbradshaw@claflin.edu>
2022-07-19 17:47:27 -0400

    Added block objects to test.js for testing purposes

--- dev/test.js
+++ dev/test.js
@@ -3,5 +3,7 @@ const Blockchain = require('./blockchain');
 const bitcoin = new Blockchain();
 
 bitcoin.createNewBlock(2389, 'OINA90SDNF90N', '90ANSD9F0N9009N');
+bitcoin.createNewBlock(3782, '3O84F4O4HOVTG', 'O48F3OJ3O4CJGHN');
+bitcoin.createNewBlock(4971, '95U43F8HC99EG', '5EUFR8TJ9RCRWJR');
 
 console.log(bitcoin);
