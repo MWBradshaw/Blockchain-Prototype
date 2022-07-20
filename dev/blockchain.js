c13b1dc0cdcdc8e5dc4e8b1bf5be00ea9df6d022
MWBradshaw <mbradshaw@claflin.edu>
2022-07-19 20:36:21 -0400

    This commit will add the createNewTransaction method to blockchain.js

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,6 +1,6 @@
 function Blockchain() {
     this.chain = [];
-    this.newTransactions = [];
+    this.pendingTransactions = [];
 }
 
 
@@ -8,16 +8,32 @@ Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash) {
     const newBlock = {
         index: this.chain.length + 1,
         timestamp: Date.now(),
-        transactions: this.newTransactions,
+        transactions: this.pendingTransactions,
         nonce: nonce,
         hash: hash, 
         previousBlockHash: previousBlockHash
     };
 
-    this.newTransactions = [];
+    this.pendingTransactions = [];
     this.chain.push(newBlock);
 
     return newBlock;
 }
 
 module.exports = Blockchain;
+
+Blockchain.prototype.getLastBlack = function() {
+    return this.chain[this.chain.length - 1];
+}
+
+Blockchain.prototype.createNewTransaction = function(amount, sender, recipient) {
+    const newTransaction = {
+        amount: amount, 
+        sender: sender, 
+        recipient: recipient
+    };
+
+    this.pendingTransactions.push(newTransaction);
+
+    return this.getLastBlock()['index'] + 1;
+}
--- dev/test.js
+++ dev/test.js
@@ -3,7 +3,13 @@ const Blockchain = require('./blockchain');
 const bitcoin = new Blockchain();
 
 bitcoin.createNewBlock(2389, 'OINA90SDNF90N', '90ANSD9F0N9009N');
+bitcoin.createNewTransaction(100, 'ALEX93497495989', 'JENN3484438849')
 bitcoin.createNewBlock(3782, '3O84F4O4HOVTG', 'O48F3OJ3O4CJGHN');
+
+bitcoin.createNewTransaction(50, 'ALEX93497495989', 'JENN3484438849')
+bitcoin.createNewTransaction(300, 'ALEX93497495989', 'JENN3484438849')
+bitcoin.createNewTransaction(2000, 'ALEX93497495989', 'JENN3484438849')
 bitcoin.createNewBlock(4971, '95U43F8HC99EG', '5EUFR8TJ9RCRWJR');
 
-console.log(bitcoin);
+
+console.log(bitcoin.chain[1]);
